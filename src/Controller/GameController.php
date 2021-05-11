abba9bbd7c9240cad82a4f01629d79de44e00234
aurelien <vannier.autrelien2@gmail.com>
2021-05-11 21:40:09 +0200

    gamecontroller with game + view not pretty

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -9,7 +9,7 @@
 
 namespace App\Controller;
 
-class GAmeController extends AbstractController
+class GameController extends AbstractController
 {
     /**
      * Display home page
@@ -25,11 +25,60 @@ class GAmeController extends AbstractController
         'img' => 'assets/images/game/Percyx600.jpg'];
         $robotGinny = ['name' => 'Ginny','attacks' => ['Coup d\'hélice', 'Charge', 'Réfléxion solaire'],
         'img' => 'assets/images/game/Ginnyx600.jpg'];
-        $cardEpmpty = ['name' => ''];
-        $robots = [$robotPercy, $cardEpmpty, $robotGinny];
+        $cardEmpty = ['name' => ''];
+        $robots = [$robotPercy, $cardEmpty, $robotGinny];
 
         return $this->twig->render('Game/index.html.twig', [
             'robots' => $robots
+        ]);
+    }
+
+    public function readyToGo(string $robotSelected)
+    {
+        $robotPercy = ['name' => 'Percy','attacks' => ['Coup de caméra', 'Rouler', 'Lanceur de filet'],
+                    'img' => 'assets/images/game/Percyx600.jpg'];
+        $robotGinny = ['name' => 'Ginny','attacks' => ['Coup d\'hélice', 'Charge', 'Réfléxion solaire'],
+                    'img' => 'assets/images/game/Ginnyx600.jpg'];
+        $gifAttacks = ['camera.gif' => 'Coup de caméra',
+                    'roll.gif' => 'Rouler',
+                    'net.gif' => 'Lanceur de filet',
+                    'helix.gif' => 'Coup d\'hélice',
+                    'charge.gif' => 'Charge',
+                    'solar.gif' => 'Réfléxion solaire'
+                    ];
+
+        $attackPercy = $robotPercy['attacks'][rand(0, 2)];
+        $attackGinny = $robotGinny['attacks'][rand(0, 2)];
+        $attackPercyImg = array_search($attackPercy, $gifAttacks);
+        $attackGinnyImg = array_search($attackGinny, $gifAttacks);
+
+        $ginny = ['name' => 'Ginny', 'attack' => $attackGinny, 'img' => 'assets/images/weapons/' . $attackGinnyImg];
+        $percy = ['name' => 'Percy', 'attack' => $attackPercy, 'img' => 'assets/images/weapons/' . $attackPercyImg];
+
+        if ($attackGinny === 'Réfléxion solaire' && $attackPercy === 'Coup de caméra') {
+            $robotWin = $ginny;
+            $robotLose = $percy;
+        } elseif ($attackGinny === 'Charge' && $attackPercy === 'Rouler') {
+            $robotWin = $ginny;
+            $robotLose = $percy;
+        } elseif ($attackGinny === 'Coup d\'hélice' && $attackPercy === 'Lanceur de filet') {
+            $robotWin = $ginny;
+            $robotLose = $percy;
+        } else {
+            $robotWin = $percy;
+            $robotLose = $ginny;
+        }
+
+        if ($robotSelected === $robotWin['name']) {
+            $userWinOrNot = 'Vous avez gagné. &#128079';
+        } else {
+            $userWinOrNot = 'Vous avez misé sur le mauvais cheval. Essaye encore &#128512';
+        }
+
+        return $this->twig->render('Game/gameWinner.html.twig', [
+            'robotWin' => $robotWin,
+            'robotLose' => $robotLose,
+            'UserWinOrNot' => $userWinOrNot
         ]);
     }
 }
