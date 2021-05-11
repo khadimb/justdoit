804682121be78e824061825362ad8934f7b800aa
wilder <NEL45>
2021-05-11 22:32:07 +0200

    carroussel test

--- src/Controller/EnvironmentController.php
+++ src/Controller/EnvironmentController.php
@@ -1,23 +1,22 @@
 <?php
 
 namespace App\Controller;
+
 use Symfony\Component\HttpClient\HttpClient;
 
 class EnvironmentController extends AbstractController
 {
     public function environment()
     {
-
+        $key = 'Uhb88PepnBJsmemDorgenrrjO1qUIovFepIVw5bR';
         $client = HttpClient::create();
-        $response = $client->request('GET', 'http://mars-photos.herokuapp.com/api/v1/rovers/Perseverance/photos?earth_date=2021-05-09?api_key=Uhb88PepnBJsmemDorgenrrjO1qUIovFepIVw5bR');
-        $selectMaps = [];
-
+        $response = $client->request('GET', 'http://mars-photos.herokuapp.com/
+            api/v1/rovers/Perseverance/photos?earth_date=2021-05-09?api_key=' . $key);
         $statusCode = $response->getStatusCode();
-
+        $maps = [];
         if ($statusCode === 200) {
             $maps = $response->toArray();
         }
-        
         return $this->twig->render('Environment/environment.html.twig', ['maps' => $maps['photos']]);
     }
 }
