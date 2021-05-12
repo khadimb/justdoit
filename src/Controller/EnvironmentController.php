58ad134eea4fd23489912dc348833e51b906258c
ChristianOlmedo <christian.olmedo0210@gmail.com>
2021-05-12 11:08:26 +0200

    fix

--- src/Controller/EnvironmentController.php
+++ src/Controller/EnvironmentController.php
@@ -10,16 +10,14 @@ class EnvironmentController extends AbstractController
     {
         $key = 'Uhb88PepnBJsmemDorgenrrjO1qUIovFepIVw5bR';
         $client = HttpClient::create();
-        $response = $client->request('GET', 'http://mars-photos.herokuapp.com/
-        api/v1/rovers/Perseverance/photos?earth_date=2021-05-09?api_key=' . $key);
+        $response = $client->request('GET', 'http://mars-photos.herokuapp.com/api/v1/rovers/Perseverance/photos?earth_date=2021-05-09?api_key=' . $key);
         $statusCode = $response->getStatusCode();
         $maps = ['photos'];
 
         if ($statusCode === 200) {
             $maps = $response->toArray();
         }
-        $maps = [$maps['photos']['33'], $maps['photos']['265'],
-        $maps['photos']['203'], $maps['photos']['269'], $maps['photos']['103']];
+        $maps = [$maps['photos']['33'], $maps['photos']['265'], $maps['photos']['203'], $maps['photos']['269'], $maps['photos']['103'], $maps['photos']['5'], $maps['photos']['77'], $maps['photos']['56']];
         return $this->twig->render('Environment/environment.html.twig', ['maps' => $maps]);
     }
 }
